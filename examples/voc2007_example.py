c9011aaf07d55e20234b2ef0647d3d7119b2f973
Vijaysrinivas Rajagopal <vijaythinks@gmail.com>
2022-02-13 00:43:51 -0500

    added aps per class

--- examples/voc2007_example.py
+++ examples/voc2007_example.py
@@ -1,5 +1,6 @@
 import os
 import sys
+sys.path.append('../')
 import tidecv
 from tidecv.drivers.fileBB import TIDE_FromListFolder
 from tidecv.quantify import TIDE
@@ -9,6 +10,6 @@ detectionFolder = './voc2007/det'
 testFile = './voc2007/2007_test.txt'
 gtData, detData = TIDE_FromListFolder(testFile, groundTruthFolder, detectionFolder)
 tide = TIDE()
-tide.evaluate_range(gtData, detData)
+tide.evaluate(gtData, detData)
 tide.summarize()
 tide.plot()
